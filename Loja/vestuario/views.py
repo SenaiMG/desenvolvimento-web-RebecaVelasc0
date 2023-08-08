5b7ba8e2f7f081062b53a3896ec2321154d1d6c3
Rebeca <rebecavelasco2020@gmail.com>
2023-08-08 20:23:02 -0300

    extend finalizado

--- Loja/vestuario/views.py
+++ Loja/vestuario/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
-    return HttpResponse('<h1>teste</h1>')
+    return HttpResponse('<h1>/pagina /catalogo</h1>')
 
 def pagina(request):
     return render(request, 'index.html')
